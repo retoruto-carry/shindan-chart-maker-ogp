95985568f19491107dfbb386b3da678ffd9407c5
retoruto-carry <k.yuta.happy.123456789@gmail.com>
2020-06-12 22:07:41 +0900

    :+1: add canvas

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,24 @@
-// api/index.ts
-import { NowRequest, NowResponse } from "@now/node";
+import * as path from 'path'
+import { NowRequest, NowResponse } from '@now/node'
+const { createCanvas, registerFont } = require('canvas')
 
-export default (req: NowRequest, res: NowResponse) => {
-  res.status(200).send("Hello Vercel!!");
-};
+export default function(_req: NowRequest, res: NowResponse) {
+  registerFont(path.join('fonts', 'rounded-mplus-1p-medium.ttf'), {
+    family: 'rounded-mplus-1p-medium'
+  })
+
+  const canvas = createCanvas(600, 315)
+  const context = canvas.getContext('2d')
+
+  context.font = '15px rounded-mplus-1p-medium'
+  context.fillStyle = '#424242'
+  context.fillText('hello', 100, 100)
+
+  const image = canvas.toBuffer()
+
+  res.writeHead(200, {
+    'Content-Type': 'image/png',
+    'Content-Length': image.length
+  })
+  res.end(image, 'binary')
+}
